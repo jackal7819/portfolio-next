47ee46350b12601a0dfd8d8185788cd2dcafa2c2
jackal7819 <jackal3166@gmail.com>
2024-12-08 22:27:00 +0200

    fix: remove not working links 12/24

--- data.js
+++ data.js
@@ -20,11 +20,11 @@ export const data = [
 	{
 		id: 3,
 		color: 'from-cyan-500 to-blue-500',
-		title: 'Liquid Lab',
-		desc: 'LiquidLab is a SPA designed to introduce users to a delightful collection of cocktail recipes. Technologies Used - JavaScript, React, React Router, React Toastify, Styled-Components, Tanstack Query, Vite, TheCocktailDB API.',
-		img: '/react-liquid.jpg',
-		code: 'https://github.com/jackal7819/liquid-lab',
-		link: 'https://liquid-lab.onrender.com/',
+		title: 'GitHub Persona',
+		desc: `GitHub Persona is a web application that provides information about GitHub users. Users can log in using their GitHub credentials via the login page, view profile data, repositories, programming languages, followers, and starred repositories.`,
+		img: '/react-persona.png',
+		code: 'https://github.com/jackal7819/github-persona',
+		link: 'https://github-persona.onrender.com/',
 	},
 	{
 		id: 4,
@@ -47,15 +47,6 @@ export const data = [
 	{
 		id: 6,
 		color: 'from-cyan-500 to-blue-500',
-		title: 'GitHub Persona',
-		desc: `GitHub Persona is a web application that provides information about GitHub users. Users can log in using their GitHub credentials via the login page, view profile data, repositories, programming languages, followers, and starred repositories.`,
-		img: '/react-persona.png',
-		code: 'https://github.com/jackal7819/github-persona',
-		link: 'https://github-persona.onrender.com/',
-	},
-	{
-		id: 7,
-		color: 'from-cyan-500 to-blue-500',
 		title: 'CINE World',
 		desc: `CINE World is a movie database website built using the TMDB API. It allows users to view information about movies, ratings, what's currently on top. Technologies Used - TypeScript, Next.js, React, Tailwind CSS, TMDB API.`,
 		img: '/next-cine.jpg',
@@ -63,7 +54,7 @@ export const data = [
 		link: 'https://imdb-clone-seven-roan.vercel.app/',
 	},
 	{
-		id: 8,
+		id: 7,
 		color: 'from-cyan-500 to-blue-500',
 		title: 'Gaming Horizon',
 		desc: 'Gaming Horizon - your unique video game store where you can enjoy a wide selection of games, add them to your favorites, create orders, and much more! Technologies Used - JavaScript, Vue.js, Tailwind CSS, Axios, Firebase, Formkit Auto Animate, Vite.',
@@ -72,7 +63,7 @@ export const data = [
 		link: 'https://gaming-horizon.onrender.com/',
 	},
 	{
-		id: 9,
+		id: 8,
 		color: 'from-cyan-500 to-blue-500',
 		title: 'Pinia Tasks',
 		desc: 'Pinia Tasks is a simple task management application. You can add new tasks, delete them, mark them as favorites, display all tasks or only favorites, and also mark tasks as completed. Technologies Used - JavaScript, Vue.js, Tailwind CSS, Axios, Firebase, Nanoid, Vite.',
@@ -81,7 +72,7 @@ export const data = [
 		link: 'https://pinia-tasks-c22l.onrender.com/',
 	},
 	{
-		id: 10,
+		id: 9,
 		color: 'from-cyan-500 to-blue-500',
 		title: 'Cinematica',
 		desc: 'This is a movie application that allows users to browse popular, top-rated, and upcoming movies. The data for movies is fetched from The Movie Database. Technologies Used - JavaScript, Nuxt.js, Vue.js, Tailwind CSS, Axios, Vite, TMDB API.',
@@ -90,12 +81,21 @@ export const data = [
 		link: 'https://cinematica-jade.vercel.app/',
 	},
 	{
-		id: 11,
+		id: 10,
 		color: 'from-cyan-500 to-blue-500',
 		title: 'CRM System',
 		desc: 'The CRM (Customer Relationship Management) system is a web application that provides a comprehensive solution for managing customer relationships, deals, and sales activities. Technologies Used - TypeScript, Nuxt.js, Vue.js, Pinia, Tailwind CSS, Vue Query, Appwrite, Vite.',
 		img: '/nuxt-crm.jpg',
 		code: 'https://github.com/jackal7819/crm-system',
 		link: 'https://crm-system-nine.vercel.app',
+	},
+	{
+		id: 11,
+		color: 'from-cyan-500 to-blue-500',
+		title: 'Liquid Lab',
+		desc: 'Sushi and rolls delivery application for the Golden Dragon restaurant. Technologies Used - JavaScript, CSS, React, Hooks - useState, useEffect, useContext, useRef, useReducer, Firebase.',
+		img: '/react-dragon.png',
+		code: 'https://github.com/jackal7819/golden-dragon',
+		link: 'https://goldendragon.onrender.com/',
 	},
 ];
